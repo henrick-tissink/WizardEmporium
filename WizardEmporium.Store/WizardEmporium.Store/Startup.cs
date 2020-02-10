442503c557473f8125d43174795b3ead3517efa8
Henrick <hltissink@gmail.com>
2020-02-10 23:13:34 +0200

    Added swagger

--- WizardEmporium.Store/WizardEmporium.Store/Startup.cs
+++ WizardEmporium.Store/WizardEmporium.Store/Startup.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using WizardEmporium.Common.SharedSwaggerConfig;
 
 namespace WizardEmporium.Store
 {
@@ -16,6 +13,14 @@ namespace WizardEmporium.Store
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            // Swagger generation
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Wizard Emporium Store API", Version = "v1" });
+                c.OperationFilter<DescribeErrorTypeOnResponseOperationFilter>();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +33,17 @@ namespace WizardEmporium.Store
 
             app.UseRouting();
 
+            // Swagger generation
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("swagger/v1/swagger.json", "Wizard Emporium Store API");
+                options.RoutePrefix = string.Empty;
+            });
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
