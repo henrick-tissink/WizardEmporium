c25c037d1926f6faf9091a248b1668eb9705c1c6
Henrick <hltissink@gmail.com>
2020-02-26 10:22:13 +0200

    smol fixes

--- WizardEmporium/WizardEmporium.Delivery/Controller/DeliveryController.cs
+++ WizardEmporium/WizardEmporium.Delivery/Controller/DeliveryController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using WizardEmporium.Common.SharedController;
 using WizardEmporium.Common.SharedResponse;
+using WizardEmporium.Deliveries.Dto;
 using WizardEmporium.Deliveries.Service;
 using WizardEmporium.Deliveries.ServiceObject;
 
@@ -17,17 +19,26 @@ namespace WizardEmporium.Deliveries.Controller
             this.service = service;
         }
 
-        [HttpGet("Deliveries")]
+        [HttpGet("All")]
+        [ProducesResponseType(typeof(ValueResponse<IEnumerable<DeliveryDto>, DeliveryServiceResponseCode>), 200)]
         [ProducesResponseType(typeof(ErrorResponse), 400)]
         public async Task<IActionResult> GetDeliveries() =>
             PrepareResponse(await service.GetDeliveriesAsync());
 
+        [HttpGet("{deliveryId}")]
+        [ProducesResponseType(typeof(ValueResponse<DeliveryDto, DeliveryServiceResponseCode>), 200)]
+        [ProducesResponseType(typeof(ErrorResponse), 400)]
+        public async Task<IActionResult> GetDelivery(int deliveryId) =>
+            PrepareResponse(await service.GetDeliveryAsync(deliveryId));
+
         [HttpPost("Schedule")]
+        [ProducesResponseType(typeof(EmptyResponse<DeliveryServiceResponseCode>), 200)]
         [ProducesResponseType(typeof(ErrorResponse), 400)]
         public async Task<IActionResult> ScheduleDelivery(ScheduleDeliveryRequest request) =>
             PrepareResponse(await service.ScheduleDelivery(request));
 
         [HttpPut("Complete/{deliveryId}")]
+        [ProducesResponseType(typeof(EmptyResponse<DeliveryServiceResponseCode>), 200)]
         [ProducesResponseType(typeof(ErrorResponse), 400)]
         public async Task<IActionResult> CompleteDelivery(int deliveryId) =>
             PrepareResponse(await service.CompleteDeliveryAsync(deliveryId));
--- WizardEmporium/WizardEmporium.Gateway/Startup.cs
+++ WizardEmporium/WizardEmporium.Gateway/Startup.cs
@@ -14,14 +14,11 @@ namespace WizardEmporium.Gateway
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,14 +27,6 @@ namespace WizardEmporium.Gateway
             }
 
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
 
             app.UseOcelot().Wait();
         }
